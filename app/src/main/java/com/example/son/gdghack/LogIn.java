d216e7f4740055909de69462667bb4ba867dd9b6
Nguyen Van Son <nvs266@gmail.com>
2017-11-18 13:03:11 +0700

    Add login feature

--- app/src/main/java/com/example/son/gdghack/LogIn.java
+++ app/src/main/java/com/example/son/gdghack/LogIn.java
@@ -3,6 +3,7 @@ package com.example.son.gdghack;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,11 +14,11 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity implements View.OnClickListener{
 
     Button bnLogIn;
     EditText etUserName;
-    EditText password;
+    EditText etPassword;
 
     private FirebaseAuth mAuth;
 
@@ -33,25 +34,38 @@ public class LogIn extends AppCompatActivity {
 
     private void setupUI() {
         bnLogIn = findViewById(R.id.login);
+        etPassword = findViewById(R.id.password);
+        etUserName = findViewById(R.id.name);
     }
 
-    public void signIn(String email, String password) {
+    public void signIn() {
+        String email = "";
+        String password = "";
+
+        email = "" + etUserName.getText();
+        password = "" + etPassword.getText();
+
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
                             FirebaseUser user = mAuth.getCurrentUser();
                             Toast.makeText(LogIn.this, "Success", Toast.LENGTH_SHORT).show();
                         } else {
-                            // If sign in fails, display a message to the user.
                             Toast.makeText(LogIn.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                         }
-
-                        // ...
                     }
                 });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.login:
+                signIn();
+                break;
+        }
     }
 }
